82306b68d50f8b2ed9af07c6846e861e73dea5e7
Nicholas Teague <44011748+Automunge@users.noreply.github.com>
2025-05-03 02:42:12 -0400

    Update load.py

--- LLM_context/load.py
+++ LLM_context/load.py
@@ -1,6 +1,6 @@
 
 #this file will provide process 
 #for loading the context of this repo
-#into an LLM model used as basis for vine coding interactions
-#the intent is that the context loading will be refreshe
+#into an LLM model used as basis for vibe coding interactions
+#the intent is that the context loading will be refreshed
 #each time that the repo is updated by a git commit
