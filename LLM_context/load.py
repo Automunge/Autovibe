a79aa552c26653a7ba5fe35c4d080b7e8070cd39
Nicholas Teague <44011748+Automunge@users.noreply.github.com>
2025-05-03 20:24:40 -0400

    Update load.py

--- LLM_context/load.py
+++ LLM_context/load.py
@@ -4,3 +4,5 @@
 #into an LLM model used as basis for vibe coding interactions
 #the intent is that the context loading will be refreshed
 #each time that the repo is updated by a git commit
+
+print('load.py complete')
